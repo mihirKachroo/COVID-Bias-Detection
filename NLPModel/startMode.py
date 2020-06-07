c6a90b18144dec3e3fb030a498f65bbaa1f83303
mihirKachroo <60433268+mihirKachroo@users.noreply.github.com>
2020-06-07 12:10:43 -0400

    dsf

--- NLPModel/startMode.py
+++
@@ -1,4 +0,0 @@
-import bias
-print("check 1")
-statement = "Hello, my name is mihir. Trump is the worst president ever. Trump should give up office. He is a horrible person and does not deserve it."
-print(bias.compute_bias(statement))
--- README.md
+++
@@ -1,3 +0,0 @@
-# COVID Bias Detection Website
-Hackathon project for Inovation In Stem Hackathon
-Created Mihir and Dhir Kachroo
