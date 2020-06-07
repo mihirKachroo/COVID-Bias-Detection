9b5e77cf614d670e4323f924d7f0491893d8c811
mihirKachroo <60433268+mihirKachroo@users.noreply.github.com>
2020-06-06 23:02:04 -0400

    dsd

---
+++ NLPModel/startMode.py
@@ -0,0 +1,4 @@
+import bias
+print("check 1")
+statement = "Hello, my name is mihir. Trump is the worst president ever. Trump should give up office. He is a horrible person and does not deserve it."
+print(bias.compute_bias(statement))
